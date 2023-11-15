1ba1da03119aedf6a0583582ab59d64e8788f933
Matthew Grady <matthewbgrady90@gmail.com>
2023-11-14 21:57:38 -0500

    Text logo ready.

--- src/components/spa-text-logo/spa-text-logo.tsx
+++ src/components/spa-text-logo/spa-text-logo.tsx
@@ -3,10 +3,6 @@ import { Component, Prop, h } from '@stencil/core';
 @Component({
   tag: 'spa-text-logo',
   styleUrl: 'spa-text-logo.css',
-  styles: [
-    'spa-text-logo.css',
-    'web-components.css'
-  ],
   shadow: true,
 })
 export class SpaTextLogo {
