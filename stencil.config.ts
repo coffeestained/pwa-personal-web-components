67e6ebb67ba46687ab1009c7dd964c4170891196
Matthew Grady <matthewbgrady90@gmail.com>
2023-12-13 22:51:13 -0500

    Progress

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,5 +1,4 @@
 import { Config } from '@stencil/core';
-import { angularOutputTarget } from '@stencil/angular-output-target';
 import { sass } from '@stencil/sass';
 
 export const config: Config = {
@@ -10,33 +9,11 @@ export const config: Config = {
   ],
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-    },
-    {
       type: 'dist-custom-elements',
       customElementsExportBehavior: 'auto-define-custom-elements',
       dir: "dist/custom-elements/"
       // Output target config options here
     },
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
-      dir: "dist/www"
-    },
-    // By default, the generated proxy components will
-    // leverage the output from the `dist` target, so we
-    // need to explicitly define that output alongside the
-    // Angular target
-    angularOutputTarget({
-      componentCorePackage: 'stencil-library',
-      outputType: 'component',
-      directivesProxyFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/components.ts',
-      directivesArrayFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/index.ts',
-    }),
   ],
   testing: {
     browserHeadless: "new",
